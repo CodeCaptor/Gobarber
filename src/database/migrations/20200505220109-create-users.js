db2a067146e52e58d58f01760b53e8b25ce0e224
KZTN <kakaiocesar2@gmail.com>
2020-05-05 19:19:52 -0300

    :sparkles: Created first database user migration
    all changes are in progress

---
+++ src/database/migrations/20200505220109-create-users.js
@@ -0,0 +1,43 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('users', {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNUll: false,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNUll: false,
+                unique: true,
+            },
+            password_hash: {
+                type: Sequelize.STRING,
+                allowNUll: false,
+                unique: true,
+            },
+            provider: {
+                type: Sequelize.BOOLEAN,
+                defaltValue: false,
+                allowNUll: false,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNUll: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNUll: false,
+            },
+        });
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('users');
+    },
+};
