e9f145e56078e87b410ccbfb311eeb784fcd7860
KZTN <kakaiocesar2@gmail.com>
2020-05-05 22:52:34 -0300

    :lock: Fixed securities about schema validation
    all changes and dependencies is due this commit content

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,9 +1,18 @@
 /* eslint-disable class-methods-use-this */
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 import User from '../models/User';
+import authConfig from '../../config/auth';
 
 class SessionController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(),
+            password: Yup.string().required().min(6),
+        });
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'validation fails.' });
+        }
         const { email, password } = req.body;
         const user = await User.findOne({ where: { email } });
         if (!user) {
@@ -15,8 +24,8 @@ class SessionController {
         const { id, name } = user;
         return res.json({
             user: { id, name },
-            token: jwt.sign({ id }, '15f2a9f750debb571691fa47727814a0', {
-                expiresIn: '7d',
+            token: jwt.sign({ id }, authConfig.secret, {
+                expiresIn: authConfig.expiresIn,
             }),
         });
     }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: '15f2a9f750debb571691fa47727814a0',
+    expiresIn: '7d',
+};
