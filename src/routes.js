151a7dc606d37e48386e5d6f80a085df74075052
KZTN <kakaiocesar2@gmail.com>
2020-06-26 17:42:23 -0300

    :art: Added new middleware to verify authentication
    all changes are functional but still in progress

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import authMiddleware from './app/middlewares/auth'
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
@@ -7,6 +7,8 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.get('/users', UserController.index);
+routes.put('/users', authMiddleware, UserController.update);
+
 routes.post('/sessions', SessionController.store);
 
 
