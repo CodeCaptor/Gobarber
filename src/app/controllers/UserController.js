b58f953955ac77bab27bb4ca35f6a6ed8dd3e722
KZTN <kakaiocesar2@gmail.com>
2020-06-26 17:55:57 -0300

    :card_file_box: WOrking on CRUD UserController
    Added Update method with security steps

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,19 +6,32 @@ class UserController {
             where: { email: req.body.email },
         });
         if (!userExists) {
-            const {id, name, email, provider} = await User.create(req.body);
-            return res.json({id, name, email, provider});
+            const { id, name, email, provider } = await User.create(req.body);
+            return res.json({ id, name, email, provider });
         }
         return res.status(400).json({ error: 'Email aready used' });
     }
 
     async index(req, res) {
         const users = await User.findAll({});
-        return res.json({users});
+        return res.json({ users });
     }
 
     async update(req, res) {
-        return res.json({msg: 'ok'});
+        const { email, password } = req.body;
+        const user = await User.findByPk(req.userID);
+
+        if (user !== user.email) {
+            const userExists = await User.findOne({ where: { email } });
+            if (userExists) {
+                return res.status(400).json({ error: 'Email in use' });
+            }
+        }
+        if (password && !(await user.checkPassword(password))) {
+            return res.status(401).json({error: "Wrong old password"});
+        }
+        const {id, name, provider} = await user.update(req.body);
+        return res.json({id, name, provider});
     }
 }
 
