f05c5123baa91c8bc66edd4c19e9ec241217c7cd
KZTN <kakaiocesar2@gmail.com>
2020-07-18 14:30:07 -0300

    :fire: Remaking all the project
    restarting

--- src/util/FormatDate.js
+++
@@ -1,7 +0,0 @@
-import {format} from 'date-fns'
-import pt from 'date-fns/locale/pt'
-
-export default function FormatDate(date) {
-    const dateFormated = format(date, "'dia' dd 'de' MMMM', às ' H:mm'h'", {locale: pt})
-    return(dateFormated);
-}
