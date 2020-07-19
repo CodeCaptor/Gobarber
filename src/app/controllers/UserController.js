ba6b690323e512d3a26a96e4b1cbe00a09ae5ac8
KZTN <kakaiocesar2@gmail.com>
2020-07-19 00:01:15 -0300

    :lock: Added JWT security
    all changes are functional

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,7 +4,7 @@ class UserController {
   async store(req, res) {
     const userExists = await User.findOne({ where: { email: req.body.email } });
     if (userExists) {
-      return res.status(400).json({ error: 'Email already in use' });
+      return res.status(400).json({ error: 'Email already in use  ' });
     }
     const user = await User.create(req.body);
     const { id, name, email, provider } = user;
