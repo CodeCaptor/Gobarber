750745b4234c8ed2372c0294db8b88a32cb5bde5
KZTN <kakaiocesar2@gmail.com>
2020-07-19 17:47:14 -0300

    :sparkles: Added nodemailer to project
    all changes are functional

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd7a92165be8e25',
+    pass: '457d121ffe2b22',
+  },
+  default: {
+    from: 'Equipe GoBarder <noreply@gobarber.com>',
+  },
+};
